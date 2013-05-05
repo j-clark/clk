b90d6b54ed9ee17bbd96584e79afe0fb5c53cda2
Joshua Clark <joshua.clark@txstate.edu>
2013-05-05 15:29:26 -0500

    user auth mostly works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Clk::Application.routes.draw do
+  root :to => 'Home#index'
+
+  resources :users
+  resources :sessions
+  get "sessions/destroy"
+  get "logout" => "sessions#destroy", :as => "logout"
+
+  match "login" => "Sessions#new"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +64,5 @@ Clk::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
