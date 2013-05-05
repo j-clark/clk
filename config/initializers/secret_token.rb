b131c451af273be97d1dd7d4f36e096be4561d03
Joshua Clark <joshua.clark@txstate.edu>
2013-05-05 12:16:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clk::Application.config.secret_token = '81bb57041d818fc78b812bc6a9a9e39ab482950abd18d70687645f0e76ebe44131963a74cd56b4983d3bb4abf5eeafe32612578c0b925b868d9e52fbe05fdef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Clk::Application.config.session_store :cookie_store, key: '_clk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Clk::Application.config.session_store :active_record_store
